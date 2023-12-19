7e4eb94c28019b84a7d2b0e02f77c34d01a33604
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2023-12-19 19:27:06 +0000

    format

--- neovim/init.lua
+++ neovim/init.lua
@@ -35,34 +35,40 @@ vim.o.smartindent=true
 vim.o.softtabstop=4 -- Number of spaces that a <Tab> counts for while performing editing
 
 -- CMP
-require("cmp")
+require("cmp").setup {
+  sources = {
+    { name = "nvim_lsp" }
+  }
+}
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- LSP
 lspconfig = require("lspconfig")
 lspconfig.pylsp.setup {
-settings = {
-    pylsp = {
-    plugins = {
-        -- formatter options
-        black = { enabled = true },
-        autopep8 = { enabled = false },
-        yapf = { enabled = false },
-        -- linter options
-        pylint = { enabled = true, executable = "pylint" },
-        pyflakes = { enabled = false },
-        pycodestyle = { enabled = false },
-        -- type checker
-        pylsp_mypy = { enabled = true },
-        -- auto-completion options
-        jedi_completion = { fuzzy = true },
-        -- import sorting
-        pyls_isort = { enabled = true },
+    settings = {
+        pylsp = {
+            plugins = {
+                -- formatter options
+                black = { enabled = true },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+                -- linter options
+                pylint = { enabled = true, executable = "pylint" },
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+                -- type checker
+                pylsp_mypy = { enabled = true },
+                -- auto-completion options
+                jedi_completion = { fuzzy = true },
+                -- import sorting
+                pyls_isort = { enabled = true },
+            },
+        },
     },
-    },
-},
-capabilities = capabilities,
+    capabilities = capabilities
 }
 
-lspconfig.lua_ls.setup{}
+lspconfig.lua_ls.setup{
+    capabilities = capabilities
+}
