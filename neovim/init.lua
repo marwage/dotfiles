82317b064df65b47cf70433bc66f3b104c143125
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2023-12-20 10:26:09 +0000

    move lsp config to extra file

--- neovim/init.lua
+++ neovim/init.lua
@@ -6,7 +6,7 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",     -- latest stable release
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
@@ -22,95 +22,21 @@ require("lazy").setup({
 
 -- COLOURSCHEME
 vim.cmd [[colorscheme sonokai]]
-vim.cmd [[hi Normal ctermbg=NONE]] -- black background with sonokai
+vim.cmd [[hi Normal ctermbg=NONE]]       -- black background with sonokai
 
 -- OPTIONS
-vim.o.number = true
-vim.o.linebreak = true
-vim.o.showbreak = "+++"
-vim.o.showmatch = true  -- When a bracket is inserted, briefly jump to the matching one
-vim.o.ignorecase = true -- Ignore case in search patterns
-vim.o.expandtab = true  -- Use the appropriate number of spaces to insert a <Tab>
-vim.o.shiftwidth = 4
-vim.o.smartindent = true
-vim.o.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing
-
--- CMP
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-local luasnip = require("luasnip")
-
--- nvim-cmp setup
-local cmp = require("cmp")
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),     -- Up
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),      -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
--- LSP
-local lspconfig = require("lspconfig")
-lspconfig.pylsp.setup {
-  settings = {
-    pylsp = {
-      plugins = {
-        -- formatter options
-        black = { enabled = true },
-        autopep8 = { enabled = false },
-        yapf = { enabled = false },
-        -- linter options
-        pylint = { enabled = true, executable = "pylint" },
-        pyflakes = { enabled = false },
-        pycodestyle = { enabled = false },
-        -- type checker
-        pylsp_mypy = { enabled = true },
-        -- auto-completion options
-        jedi_completion = { fuzzy = true },
-        -- import sorting
-        pyls_isort = { enabled = true },
-      },
-    },
-  },
-  capabilities = capabilities,
-}
-
-lspconfig.lua_ls.setup {
-  capabilities = capabilities,
-}
+vim.opt.number = true
+vim.opt.linebreak = true
+vim.opt.showbreak = "+++"
+vim.opt.showmatch = true  -- When a bracket is inserted, briefly jump to the matching one
+vim.opt.ignorecase = true -- Ignore case in search patterns
+vim.opt.expandtab = true  -- Use the appropriate number of spaces to insert a <Tab>
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing
 
 -- FORMAT ON SAVE
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+
+-- lSP
+require("lsp")
