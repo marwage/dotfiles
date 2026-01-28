95453c4439f77b6f4b467a081c4dd01a20b0dc21
Marcel Wagenländer <marcel.wagenlander19@imperial.ac.uk>
2026-01-28 10:47:31 +0000

    add LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -49,6 +49,10 @@
             ];
 
             shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc.lib
+                  pkgs.zlib
+                ]}:$LD_LIBRARY_PATH"
               export SHELL=${pkgs.zsh}/bin/zsh
               echo "Nix dev environment loaded!"
               exec zsh
